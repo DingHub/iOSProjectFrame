440cc5eb80bf1ff707bd7d8ae0619f715f3b9742
DingHub <love-nankai@163.com>
2016-03-30 11:30:42 +0800

    add BaseWebViewController
    BaseWebViewController is a sub class of BaseViewController, it’s view
    is a webView. We can pass a simple url to it.

---
+++ Common/BaseWebViewController.h
@@ -0,0 +1,15 @@
+//
+//  BaseWebViewController.h
+//  iOSProjectFrame
+//
+//  Created by admin on 16/3/30.
+//  Copyright © 2016年 Ding. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface BaseWebViewController : BaseViewController
+
+@property (nonatomic, copy) NSString *urlString;
+
+@end
---
+++ Common/BaseWebViewController.m
@@ -0,0 +1,45 @@
+//
+//  BaseWebViewController.m
+//  iOSProjectFrame
+//
+//  Created by admin on 16/3/30.
+//  Copyright © 2016年 Ding. All rights reserved.
+//
+
+#import "BaseWebViewController.h"
+
+@interface BaseWebViewController () <UIWebViewDelegate>
+
+@property (nonatomic, strong) UIWebView *webView;
+
+@end
+
+@implementation BaseWebViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.webView = [[UIWebView alloc] init];
+    self.view = _webView;
+    self.webView.delegate = self;
+    if (_urlString) {
+        [_webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:_urlString]]];
+    }
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+- (void)webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error {
+    
+}
+
+- (void)webViewDidStartLoad:(UIWebView *)webView {
+    
+}
+
+- (void)webViewDidFinishLoad:(UIWebView *)webView {
+    self.navigationItem.title = [webView stringByEvaluatingJavaScriptFromString:@"document.title"];
+}
+
+@end
